81c42891ff9483037240c613c50ed5383db6b4ab
Tom Nielsen <tanielsen@gmail.com>
2022-12-01 09:25:13 +0000

    flexible date format

--- index.js
+++ index.js
@@ -7,28 +7,30 @@ const {
   text_attr,
 } = require("@saltcorn/markup/tags");
 const range_filter = require("./date-range-filter");
-const base_headers = `/plugins/public/flatpickr-date@${require("./package.json").version}`
+const base_headers = `/plugins/public/flatpickr-date@${
+  require("./package.json").version
+}`;
 const headers = [
   {
-    script: `${base_headers}/flatpickr.min.js`
+    script: `${base_headers}/flatpickr.min.js`,
   },
   {
-    script: `${base_headers}/l10n/es.min.js`
+    script: `${base_headers}/l10n/es.min.js`,
   },
   {
-    script: `${base_headers}/l10n/pt.min.js`
+    script: `${base_headers}/l10n/pt.min.js`,
   },
   {
-    script: `${base_headers}/l10n/fr.min.js`
+    script: `${base_headers}/l10n/fr.min.js`,
   },
   {
-    script: `${base_headers}/l10n/it.min.js`
+    script: `${base_headers}/l10n/it.min.js`,
   },
   {
-    script: `${base_headers}/l10n/de.min.js`
+    script: `${base_headers}/l10n/de.min.js`,
   },
   {
-    script: `${base_headers}/l10n/ru.min.js`
+    script: `${base_headers}/l10n/ru.min.js`,
   },
   {
     css: `${base_headers}/flatpickr.min.css`,
@@ -53,7 +55,7 @@ const flatpickr = {
     {
       name: "minDate",
       label: "Min date (Ex: 2022-10-1 or today)",
-      type: "String"
+      type: "String",
     },
     // { Lo dejo comentado para mejoras a futuro
     //   name: "maxDate",
@@ -63,27 +65,28 @@ const flatpickr = {
     {
       name: "locale",
       label: "Language (locale) available: es, pt, fr, it, ru, de",
-      type: "String"
+      type: "String",
     },
     {
       name: "dateFormat",
       label: "Date format",
       required: true,
       type: "String",
-      attributes: { options: ["d-m-Y", "Y-m-d"] }
-    }
+      default: "Y-m-d H:i",
+      sublabel: `<a href="https://flatpickr.js.org/formatting/">Formatting options</a>`,
+    },
   ],
   run: (nm, v, attrs, cls) => {
     const rndid = Math.floor(Math.random() * 16777215).toString(16);
     const opts = {
       enableTime: !attrs.day_only,
       allowInput: attrs.allow_input,
-      dateFormat: attrs.day_only ? attrs.dateFormat : "Z",
+      dateFormat: attrs.day_only ? "Y-m-d" : "Z",
       altInput: !attrs.day_only,
-      altFormat: attrs.dateFormat + " h:i K",
+      altFormat: attrs.dateFormat || "Y-m-d H:i",
       minDate: attrs.minDate,
       //maxDate: attrs.maxDate,
-      locale: attrs.locale
+      locale: attrs.locale,
     };
     return (
       input({
@@ -94,10 +97,10 @@ const flatpickr = {
         id: `input${text_attr(nm)}${rndid}`,
         ...(typeof v !== "undefined" &&
           v !== null && {
-          value: text_attr(
-            typeof v === "string" ? v : v ? v.toISOString() : undefined
-          ),
-        }),
+            value: text_attr(
+              typeof v === "string" ? v : v ? v.toISOString() : undefined
+            ),
+          }),
       }) +
       script(
         domReady(
